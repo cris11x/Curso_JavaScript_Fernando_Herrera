0cd00ef25cd9bcbcf2652319f848a760300def69
Cristian Fajardo <cris10app@gmail.com>
2021-01-04 09:12:01 -0500

    Proptip

--- 01-FUNDAMENTOS/assets/js/funciones.js
+++ 01-FUNDAMENTOS/assets/js/funciones.js
@@ -1,6 +1,10 @@
 function saludar( nombre ) {
-    console.log( arguments );
-    console.log('hola '+ nombre);
+    // console.log( arguments );
+    // console.log('hola '+ nombre);
+    return [1,2];
+
+    // Esto nunca se va a ejecutar
+    console.log('Soy un color de codigo que esta despues');
 }
 
 const saludar2 = function( nombre ) {
@@ -16,7 +20,26 @@ const saludarFlecha2 = ( nombre ) => {
 }
 
 
-saludar('cristian', 29, true, 'Colombia');
+const retornoDeSaludar = saludar('cristian', 29, true, 'Colombia'); //1
 // saludar2('cristian');
-saludarFlecha();
-saludarFlecha2('Cristian');
+// saludarFlecha();
+// saludarFlecha2('Cristian');
+
+function sumar( a, b ) {
+    return a + b;
+}
+
+// const sumar2 = (a,b) => 
+// {
+//     return a + b;
+// }
+
+const sumar2 = (a,b) =>  a + b;
+
+function getAleatorio() {
+    return Math.random();
+}
+
+const getAleatorio2 = () => Math.random();
+
+console.log( getAleatorio2() );
